1047ff1617054e2cc6f72b23f02383ae97dd69ec
tiagodevsantana <75752302+tiagodevsantana@users.noreply.github.com>
2021-05-31 11:59:53 -0300

    Create IRepositorio.cs

---
+++ IRepositorio.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace DIO.Series.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+
+        T RetornaPorIdo(int id);
+
+        void Insere (T entidade);
+
+        void Exclui (int id);
+
+        void Atualiza (int id, T entidade);
+
+        int ProximoId();
+         
+    }
+}
